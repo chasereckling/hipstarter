5f19a551a12f6113c16884afb1cbaf3d06375def
Steve Schneider & Garrett Olson <garrettdolson@gmail.com>
2015-07-23 09:11:27 -0700

    Comment out paperclip code in fundraiser model that is causing error until it can be fixed

--- app/models/fundraiser.rb
+++ app/models/fundraiser.rb
@@ -2,8 +2,8 @@ class Fundraiser < ActiveRecord::Base
   has_many :donations
   has_many :users, :through => :donations
   belongs_to :user
-  has_attached_file :picture, :styles => { :medium => "300x300>", :checkout => "128x128>", :list => "100x100" }
-  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+  #has_attached_file :picture, :styles => { :medium => "300x300>", :checkout => "128x128>", :list => "100x100" }
+  #validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
   def self.has_fundraiser
     return (current_user && current_user.fundraiser)
