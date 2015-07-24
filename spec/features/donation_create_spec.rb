338bf43c3399bba1d4231044fda9efad211acb55
Brent <student@epicodus.com>
2015-07-24 13:12:04 -0700

    pass all specs

--- spec/features/donation_create_spec.rb
+++ spec/features/donation_create_spec.rb
@@ -10,16 +10,15 @@ describe 'the stripe charge path', js: true do
 		fill_in "custom_amount", with: 10
 		click_button("Donate!")
 		sleep(2.second)
-		# stripe = page.driver.window_handles.last
-		# page.within_window stripe do
 		stripe_iframe = all('iframe[name=stripe_checkout_app]').last
-		within_frame stripe_iframe do
-			fill_in 'email', with: 'test2@test2.com'
-			fill_in 'Card number', with: '4242424242424242'
-			fill_in 'MM / YY', with: '0422'
-			fill_in 'CVC', with: '042'
-			click_on('Pay')
-		end
-		
-end
+			within_frame stripe_iframe do
+				fill_in 'email', with: 'test2@test2.com'
+				fill_in 'Card number', with: '4242424242424242'
+				fill_in 'MM / YY', with: '0422'
+				fill_in 'CVC', with: '042'
+				click_on('Pay')
+			end
+			sleep(3.second)
+			expect(page).to have_content("submitted")
+	end
 end
